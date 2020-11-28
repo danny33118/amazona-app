c7432078430e48d5d25d4b3c9ec1a12da2662709
danny mac mini <yllim888@gmail.com>
2020-11-29 00:57:25 +0800

    2nd commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import data from "./data";
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -15,45 +18,14 @@ function App() {
         </div>
       </header>
       <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-              </a>
-              <div className="card-body">
-                <a href="/product">
-          <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className={`${product.rating > 1 ? "fa fa-star" : product.rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}`}></i>
-                  </span>                  
-
-                  <span>
-                    <i className={`${product.rating > 2 ? "fa fa-star" : product.rating >= 1.5 ? "fa fa-star-half-o" : "fa fa-star-o"}`}></i>
-                  </span>                  
-
-                  <span>
-                    <i className={`${product.rating > 3 ? "fa fa-star" : product.rating >= 2.5 ? "fa fa-star-half-o" : "fa fa-star-o"}`}></i>
-                  </span>                  
-
-                  <span>
-                    <i className={`${product.rating > 4 ? "fa fa-star" : product.rating >= 3.5 ? "fa fa-star-half-o" : "fa fa-star-o"}`}></i>
-                  </span>                  
-
-                  <span>
-                    <i className={`${product.rating > 5 ? "fa fa-star" : product.rating >= 4.5 ? "fa fa-star-half-o" : "fa fa-star-o"}`}></i>
-                  </span>                  
-                </div>
-          <div className="price">${product.price}</div>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Route path="/product/:id" component={ProductScreen}/>
+        <Route path="/" exact component={HomeScreen}/>
+        
       </main>
       <footer className="row center">All Right Reserved</footer>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
